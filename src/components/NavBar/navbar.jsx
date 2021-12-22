edd328ab436de05a74026a910d991d3fecfdca87
Austin Empaynado <AustinEmpaynado@cmail.carleton.ca>
2021-12-21 22:39:17 -0500

    added nav-bar and list items

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,6 +1,17 @@
-export const NavBar = () => {
-    return(<div>
+import "./navbar.css"
 
+
+export const NavBar = () => {
+    return(
+    <div id="nav-container">
+        <div id="nav-links">
+            <li className="nav-list-item">
+                <a>Profile</a>
+            </li>
+            <li className="nav-list-item">
+                <a>Sign out</a>
+            </li>
+        </div>
         
     </div>);
 }
